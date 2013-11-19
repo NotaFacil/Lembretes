4fca53e383c4a350a806e3dcdc6f89c559087f3a
NotaFacil <notafacil.lembretes@gmail.com>
2013-11-18 22:03:18 -0200

    ok

---
+++ NotaFacil/src/com/bean/Lembrete.java
@@ -0,0 +1,163 @@
+package com.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lembrete")
+public class Lembrete implements Serializable{
+	
+	private static final long serialVersionUID = -7695012344951193407L;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "lem_id")
+	private Integer id;
+	
+	@Column(name = "lem_dh_insercao", updatable = false)
+	private Date dhCriacao;
+	
+	@Column(name = "lem_dh_inicio")
+	private Date dhInicio;
+	
+	@Column(name = "lem_dh_fim")	
+	private Date dhFim;
+	
+	@Column(name = "lem_titulo")
+	private String titulo;
+	
+	@Column(name = "lem_descricao")
+	private String descricao;
+	
+	@Column(name = "lem_local")
+	private String local;
+	
+	@ManyToOne
+	@JoinColumn(name = "usu_id")
+	private Usuario usuario;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getDhCriacao() {
+		return dhCriacao;
+	}
+	public void setDhCriacao(Date dhCriacao) {
+		this.dhCriacao = dhCriacao;
+	}
+	public Date getDhInicio() {
+		return dhInicio;
+	}
+	public void setDhInicio(Date dhInicio) {
+		this.dhInicio = dhInicio;
+	}
+	public Date getDhFim() {
+		return dhFim;
+	}
+	public void setDhFim(Date dhFim) {
+		this.dhFim = dhFim;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result
+				+ ((dhCriacao == null) ? 0 : dhCriacao.hashCode());
+		result = prime * result + ((dhFim == null) ? 0 : dhFim.hashCode());
+		result = prime * result
+				+ ((dhInicio == null) ? 0 : dhInicio.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((local == null) ? 0 : local.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lembrete other = (Lembrete) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (dhCriacao == null) {
+			if (other.dhCriacao != null)
+				return false;
+		} else if (!dhCriacao.equals(other.dhCriacao))
+			return false;
+		if (dhFim == null) {
+			if (other.dhFim != null)
+				return false;
+		} else if (!dhFim.equals(other.dhFim))
+			return false;
+		if (dhInicio == null) {
+			if (other.dhInicio != null)
+				return false;
+		} else if (!dhInicio.equals(other.dhInicio))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (local == null) {
+			if (other.local != null)
+				return false;
+		} else if (!local.equals(other.local))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
---
+++ NotaFacil/src/com/db/dao/impl/UsuarioDaoImpl.java
@@ -0,0 +1,159 @@
+package com.db.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.bean.Usuario;
+import com.db.connection.HibernateUtil;
+import com.db.dao.UsuarioDao;
+
+public class UsuarioDaoImpl implements UsuarioDao {
+
+	private Session sessao;
+	private Transaction transacao;
+
+	public void salvar(Usuario usuario) {
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			this.sessao.save(usuario);
+			this.transacao.commit();
+		} catch (HibernateException e) {
+			System.out.println("Não foi possível inserir o usuario. Erro: " + e.getMessage());
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de inserção. Mensagem: " + e.getMessage());
+			}
+		}
+	}
+
+	public void atualizar(Usuario usuario) {
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			this.sessao.update(usuario);
+			this.transacao.commit();
+		} catch (HibernateException e) {
+			System.out.println("Não foi possível alterar o usuario. Erro: " + e.getMessage());
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de atualização. Mensagem: " + e.getMessage());
+			}
+		}
+	}
+
+	public void excluir(Usuario usuario) {
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			this.sessao.delete(usuario);
+			this.transacao.commit();
+		} catch (HibernateException e) {
+			System.out.println("Não foi possível excluir o usuario. Erro: " + e.getMessage());
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de exclusão. Mensagem: " + e.getMessage());
+			}
+		}
+	}
+
+	public Usuario buscarUsuario(Integer id) {
+		Usuario usuario = null;
+
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			Criteria filtro = this.sessao.createCriteria(Usuario.class);
+			filtro.add(Restrictions.eq("id", id));
+			usuario = (Usuario) filtro.uniqueResult();
+			this.transacao.commit();
+		} catch (Throwable e) {
+			if (this.transacao.isActive()) {
+				this.transacao.rollback();
+			}
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de busca. Mensagem: " + e.getMessage());
+			}
+		}
+		return usuario;
+	}
+
+	public List<Usuario> listar() {
+		List<Usuario> usuarios = null;
+
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			Criteria filtro = this.sessao.createCriteria(Usuario.class);
+			usuarios = filtro.list();
+			this.transacao.commit();
+		} catch (Throwable e) {
+			if (this.transacao.isActive()) {
+				this.transacao.rollback();
+			}
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de listagem. Mensagem: " + e.getMessage());
+			}
+		}
+		return usuarios;
+	}
+	
+	public Usuario logar(Usuario usuario){
+		Usuario usuarioResultado = null;
+		try {
+			this.sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+			this.transacao = this.sessao.beginTransaction();
+			Criteria filtro1 = this.sessao.createCriteria(Usuario.class);
+			filtro1.add(Restrictions.eq("login", usuario.getLogin()));
+			filtro1.add(Restrictions.eq("senha", usuario.getSenha()));
+			usuarioResultado = (Usuario)filtro1.uniqueResult();
+			if(usuarioResultado == null){
+				Criteria filtro2 = this.sessao.createCriteria(Usuario.class);
+				filtro2.add(Restrictions.eq("email", usuario.getEmail()));
+				filtro2.add(Restrictions.eq("senha", usuario.getSenha()));
+				usuarioResultado = (Usuario)filtro2.uniqueResult();
+			}
+		} catch (Exception e) {
+			if (this.transacao.isActive()) {
+				this.transacao.rollback();
+			}
+
+		} finally {
+			try {
+				if (this.sessao.isOpen()) {
+					this.sessao.close();
+				}
+			} catch (Throwable e) {
+				System.out.println("Erro ao fechar operação de logar. Mensagem: " + e.getMessage());
+			}
+		}
+		return usuarioResultado;
+	}
+}
